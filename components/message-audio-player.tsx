1a73dde2465305f397d13d95addebd5c0d4705ff
Roberth González <reliutg@gmail.com>
2024-07-10 23:06:57 +0200

    Refactor MessageAudioPlayer component to use SVG animation for play and stop icons

--- components/message-audio-player.tsx
+++ components/message-audio-player.tsx
@@ -53,7 +53,111 @@ export const MessageAudioPlayer = ({
       >
         {!generatingAudio ? (
           player.isPlaying ? (
-            <StopCircleIcon size={16} />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 100 100"
+              width="24"
+              height="24"
+            >
+              <rect x="5" y="45" width="10" height="10" fill="#fff">
+                <animate
+                  attributeName="height"
+                  values="10;20;10"
+                  dur="1s"
+                  repeatCount="indefinite"
+                />
+                <animate
+                  attributeName="y"
+                  values="45;40;45"
+                  dur="1s"
+                  repeatCount="indefinite"
+                />
+              </rect>
+              <rect x="20" y="40" width="10" height="20" fill="#fff">
+                <animate
+                  attributeName="height"
+                  values="20;30;20"
+                  dur="1s"
+                  repeatCount="indefinite"
+                />
+                <animate
+                  attributeName="y"
+                  values="40;35;40"
+                  dur="1s"
+                  repeatCount="indefinite"
+                />
+              </rect>
+              <rect x="35" y="30" width="10" height="40" fill="#fff">
+                <animate
+                  attributeName="height"
+                  values="40;60;40"
+                  dur="1s"
+                  repeatCount="indefinite"
+                />
+                <animate
+                  attributeName="y"
+                  values="30;20;30"
+                  dur="1s"
+                  repeatCount="indefinite"
+                />
+              </rect>
+              <rect x="50" y="10" width="10" height="80" fill="#fff">
+                <animate
+                  attributeName="height"
+                  values="80;90;80"
+                  dur="1s"
+                  repeatCount="indefinite"
+                />
+                <animate
+                  attributeName="y"
+                  values="10;5;10"
+                  dur="1s"
+                  repeatCount="indefinite"
+                />
+              </rect>
+              <rect x="65" y="10" width="10" height="80" fill="#fff">
+                <animate
+                  attributeName="height"
+                  values="80;90;80"
+                  dur="1s"
+                  repeatCount="indefinite"
+                />
+                <animate
+                  attributeName="y"
+                  values="10;5;10"
+                  dur="1s"
+                  repeatCount="indefinite"
+                />
+              </rect>
+              <rect x="80" y="30" width="10" height="40" fill="#fff">
+                <animate
+                  attributeName="height"
+                  values="40;60;40"
+                  dur="1s"
+                  repeatCount="indefinite"
+                />
+                <animate
+                  attributeName="y"
+                  values="30;20;30"
+                  dur="1s"
+                  repeatCount="indefinite"
+                />
+              </rect>
+              <rect x="95" y="45" width="10" height="10" fill="#fff">
+                <animate
+                  attributeName="height"
+                  values="10;20;10"
+                  dur="1s"
+                  repeatCount="indefinite"
+                />
+                <animate
+                  attributeName="y"
+                  values="45;40;45"
+                  dur="1s"
+                  repeatCount="indefinite"
+                />
+              </rect>
+            </svg>
           ) : (
             <PlayIcon size={16} />
           )
