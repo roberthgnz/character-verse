bbe678b3e89ffe36ac3abe8f87adee04c9c220ad
Roberth González <reliutg@gmail.com>
2024-07-19 01:19:52 +0200

    feat: Add @radix-ui/react-toast for displaying toast notifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css"
 import { Inter as FontSans } from "next/font/google"
 
 import { cn } from "@/lib/utils"
+import { Toaster } from "@/components/ui/toaster"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -24,6 +25,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         )}
       >
         {children}
+        <Toaster />
       </body>
     </html>
   )
