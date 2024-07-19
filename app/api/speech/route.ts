a08c59bb595d142b0692986f8e1efd05b91be2df
Roberth González <reliutg@gmail.com>
2024-07-19 03:06:11 +0200

    refactor: Add rate limiting to speech route

--- app/api/speech/route.ts
+++ app/api/speech/route.ts
@@ -1,4 +1,17 @@
+import { headers } from "next/headers"
 import { characters } from "@/constants"
+import { Ratelimit } from "@upstash/ratelimit"
+
+import kv from "@/lib/kv"
+
+// Create a new ratelimiter, that allows 5 requests per 24 hours
+const ratelimit = kv
+  ? new Ratelimit({
+      redis: kv,
+      limiter: Ratelimit.fixedWindow(5, "24h"),
+      analytics: true,
+    })
+  : undefined
 
 export async function POST(req: Request) {
   const { text, character }: { text: string; character: string } =
@@ -8,6 +21,24 @@ export async function POST(req: Request) {
 
   if (!characterData) {
     return new Response("Character not found", { status: 404 })
+  }
+
+  if (ratelimit) {
+    const headersList = headers()
+
+    const ipIdentifier = headersList.get("x-real-ip")
+
+    const result = await ratelimit.limit(ipIdentifier ?? "")
+
+    if (!result.success) {
+      return new Response("Limit reached. Please try again in 24 hours.", {
+        status: 429,
+        headers: {
+          "X-RateLimit-Limit": result.limit,
+          "X-RateLimit-Remaining": result.remaining,
+        } as any,
+      })
+    }
   }
 
   const voice = await fetch("https://api.cartesia.ai/tts/bytes", {
