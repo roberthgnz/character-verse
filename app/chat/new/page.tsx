e7fb496b023e20c4d53002e40f30defdc5c91f69
Roberth González <reliutg@gmail.com>
2024-07-23 21:17:40 +0200

    refactor: Add new chat page component with character video, name, and chat form

---
+++ app/chat/new/page.tsx
@@ -0,0 +1,58 @@
+import { redirect } from "next/navigation"
+import { auth, getUser } from "@/auth"
+import { characters } from "@/constants"
+import type { Character } from "@/types"
+
+import { ChatForm } from "@/components/chat-form"
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { character: string }
+}) {
+  const character = characters.find(
+    (c) => c.name === searchParams.character
+  ) as Character | undefined
+
+  if (!character) {
+    return (
+      <div className="bg-background flex h-[calc(100vh_-_8rem)] flex-col items-center justify-center">
+        <span className="text-2xl">Character not found</span>
+      </div>
+    )
+  }
+
+  const session = await auth()
+
+  if (!session) {
+    return redirect("/login")
+  }
+
+  const user = await getUser(session!.user!.email as string)
+
+  if (!user) {
+    return redirect("/login")
+  }
+
+  return (
+    <div className="bg-background flex h-[calc(100vh_-_8rem)] flex-col items-center justify-center">
+      <div className="w-full max-w-screen-md">
+        <div className="flex flex-col items-center gap-4">
+          <video className="size-64 rounded-full" muted loop autoPlay>
+            <source
+              src={`/video/character/${character.name}.mp4`}
+              type="video/mp4"
+            />
+          </video>
+          <p className="text-lg font-semibold">{character.name}</p>
+          <div className="flex items-start gap-3">
+            <div className="bg-muted relative mx-auto max-w-[80%] rounded-lg p-3">
+              {character.defaultMessage}
+            </div>
+          </div>
+          <ChatForm userId={user.id} character={character} />
+        </div>
+      </div>
+    </div>
+  )
+}
