defa4d7c6e984b6ca8dc4f5655d26d63af1f1162
Roberth González <reliutg@gmail.com>
2024-07-23 22:34:58 +0200

    refactor: Update login page to use SignInForm component

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation"
 import { auth } from "@/auth"
 
-import LoginForm from "@/components/login-form"
+import { SignInForm } from "@/components/login-form"
 
 export const metadata = {
   title: "Login - CharacterVerse",
@@ -17,7 +17,7 @@ export default async function LoginPage() {
   return (
     <main className="bg-background flex h-[calc(100vh_-_8rem)] flex-col items-center justify-center">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <LoginForm />
+        <SignInForm />
       </div>
     </main>
   )
