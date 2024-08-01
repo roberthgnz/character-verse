34286b359089bc79cd918b1edfcb12454ff56503
Roberth González <reliutg@gmail.com>
2024-08-02 00:33:23 +0200

    refactor: Update ChatPanel component to improve user experience
    - Move the saveChatMessage function call to ensure the message is saved before appending it to the chat
    - Set focus on the input field after sending a message for better usability
    - Remove unnecessary code comments and blank lines for cleaner code

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -84,16 +84,16 @@ export const ChatPanel = ({
     setIsLoading(true)
     scrollToBottom()
 
-    // Save the message from the user
-    await saveChatMessage({ ...message, id: nanoid(7), chatId })
-
     await append(message as Message, {
       data: JSON.stringify({ characterContext: characterState }),
     })
+    await saveChatMessage({ ...message, id: nanoid(7), chatId })
 
     setInput("")
     setIsLoading(false)
     scrollToBottom()
+
+    inputRef.current?.focus()
   }
 
   const initChat = async () => {
